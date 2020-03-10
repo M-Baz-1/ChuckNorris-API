b4a1d7c5573f6617fd7307d23be5dcdc7506f820
Parrot User <user@localhost.localdomain>
2020-03-10 11:19:39 -0400

    Initialize project using Create React App

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import { Component } from 'react/cjs/react.production.min';
+
+class App extends Component {
+
+
+
+render () {
+  return (
+    <div className="App">
+      
+    </div>
+  );
+}
+}
+
+export default App;
